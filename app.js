39a3cf06a302edca024c2064d48d30930dff8999
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-06-06 13:15:05 -0300

    Update app.js
    Excluindo a functionValida e adicionando a funcConfirmar

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-function fncValida(){
-    let razaoSocial = document.getElementById("razaoSocial").value;    
-    let uf = document.querySelector("#uf");    
-        
-      if(uf.selectedIndex==0){
-        alert('Por favor, selecione o estado (UF)!');
-        return false;    
-      } 
-
-      let mensagem = document.querySelector("#mensagem");
-      mensagem.style.display = '';
-
-      let tituloMensagem = document.querySelector("#tituloMensagem");
-      tituloMensagem.innerHTML = 'O cadastro da empresa ' + razaoSocial + " foi concluído com sucesso!" ;  
-      
-    document.getElementById("paragrafo").innerHTML = `Agora poderá agendar e consultar os horários de seus clientes com mais facilidade, basta clicar em "Entrar" e fazer o seu login`;
-    
-
-    }
-
-    let btnEnviar = document.querySelector("#btnEnviar");
-    btnEnviar.addEventListener("click", function(){
-    fncValida();
-  });
-
   let foneMask = document.getElementById('fone');
   let pessoalMask = document.getElementById('fonePessoal')
   let maskOptions = {
@@ -38,5 +13,10 @@ let patternMask = IMask(cnpj, {
    mask: '00.000.000/0000-00'
    });
 
-  
+function funcConfirmar() {
+     if (confirm("Deseja confirmar o cadastro?") == true) {
+     window.alert('Seu cadastro foi concluído com sucesso!');
+     return true;
+     }
+     } 
  
