5dee18433a5557109d75f837ebc9a5d12553280e
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-06-30 11:32:19 -0300

    Update app.js
    Inclusão da api Via CEP

--- app.js
+++ app.js
@@ -18,5 +18,35 @@ function funcConfirmar() {
      window.alert('Seu cadastro foi concluído com sucesso!');
      return true;
      }
-     } 
+     }
+
+const cep = document.querySelector("#cep");
+  const options = {
+    method:'GET',
+    mode: 'cors',
+    cache: 'default'
+  }
+
+  const showData = (result) => {
+    for(const campo in result) {
+      if(document.querySelector("#"+campo)){
+        document.querySelector("#"+campo).value = result[campo];
+      }
+    }
+  }
+  cep.addEventListener("blur", (e) => {
+    let search = cep.value.replace("-", "");
+    console.log(search);
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then(response =>{
+      response.json()
+      .then(dados =>{
+        document.querySelector("#logradouro").value=dados.logradouro;
+        document.querySelector("#localidade").value=dados.localidade;
+        document.querySelector("#bairro").value=dados.bairro;
+        document.querySelector("#uf").value = dados.uf;
+        document.querySelector("#nr_end").focus();
+      })
+    })
+  })
  
