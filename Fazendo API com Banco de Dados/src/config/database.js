09aac212295bed5c2149397dbabcfad79e797990
Nayara Ferreira <104444310+nayaracferreira@users.noreply.github.com>
2022-08-03 17:05:03 -0300

    Update database.js

--- Fazendo API com Banco de Dados/src/config/database.js
+++ Fazendo API com Banco de Dados/src/config/database.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI
 const connect = async () => {
         try {
-            await mongoose.connect("mongodb+srv://nayara:ahRy6DOxfPI7zFta@cluster0.gyfsdmw.mongodb.net/?retryWrites=true&w=majority", {
+            await mongoose.connect("MONGODB_URI, {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
             })
-            console.log("Conectado a DataBase")
+            console.log("Conectando a DataBase")
         } catch (error) {
             console.error("Erro: ", error.message)
         }
